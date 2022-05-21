8932f29f770d8a1cf917292a9fd6f6574fe05314
lauratro <lauratronchin@hotmail.it>
2022-05-21 18:41:53 +0200

    Add submit button

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function InputField() {
   return (
+      <div>
    <input type="input" placeholder='Enter a task' className='input_box'/>
+   <button className="input_submit" type="submit">Go</button>
+   </div>
   )
 }
