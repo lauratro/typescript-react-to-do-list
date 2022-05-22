4d7bd2b74544b4a151a2369c376b3c7610b4cb6c
lauratro <lauratronchin@hotmail.it>
2022-05-22 12:50:05 +0200

    Import icons

--- src/components/SingleTodo/SingleTodo.tsx
+++ src/components/SingleTodo/SingleTodo.tsx
@@ -1,5 +1,8 @@
 import React from "react"
-import {Todo} from "./../../model"
+import {Todo} from "./../../model";
+import {AiFillDelete,AiFillEdit} from "react-icons/ai"
+import {MdDone} from "react-icons/md"
+
 interface Props{
 todo:Todo,
 todos:Todo[]
@@ -8,6 +11,11 @@ setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
 const SingleTodo: React.FC<Props> = ({todo, todos, setTodos})=>{
     return <form className="todos__single">
         <span className="todos__single--text">{todo.todo}</span>
+    <div>
+        <span className="icon"><AiFillDelete/></span>
+        <span className="icon"><AiFillEdit/></span>
+        <span className="icon"><MdDone/></span>
+    </div>
     </form>
 }
 export default SingleTodo;
