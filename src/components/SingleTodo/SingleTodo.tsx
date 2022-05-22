7c15623bf3f68bd4b2ed73b36338e1fcc2e0279b
lauratro <lauratronchin@hotmail.it>
2022-05-22 14:22:08 +0200

    Add function to check done Todo

--- src/components/SingleTodo/SingleTodo.tsx
+++ src/components/SingleTodo/SingleTodo.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 import {Todo} from "./../../model";
 import {AiFillDelete,AiFillEdit} from "react-icons/ai"
-import {MdDone} from "react-icons/md"
+import {MdDone} from "react-icons/md";
+import "./SingleTodo.css"
+import TodoList from "../TodoList/TodoList";
 
 interface Props{
 todo:Todo,
@@ -9,12 +11,20 @@ todos:Todo[]
 setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
 }
 const SingleTodo: React.FC<Props> = ({todo, todos, setTodos})=>{
-    return <form className="todos__single">
+  const handleDone = (id:number)=>{
+      setTodos(todos.map(todo=>(todo.id===id?{...todo, isDone:!todo.isDone}:todo)))
+  }
+  return <form className="todos__single">
+      {todo.isDone ?(
+        <s className="todos__single--text">{todo.todo}</s>
+      ):(
         <span className="todos__single--text">{todo.todo}</span>
+      )}
+        
     <div>
         <span className="icon"><AiFillDelete/></span>
         <span className="icon"><AiFillEdit/></span>
-        <span className="icon"><MdDone/></span>
+        <span className="icon" onClick={()=>handleDone(todo.id)}><MdDone/></span>
     </div>
     </form>
 }
