4347bc234a09a730527899fa8be20426fb03f92d
lauratro <lauratronchin@hotmail.it>
2022-05-22 11:50:08 +0200

    Change element to form

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,18 @@ let App: React.FC = () => {
 
   const handleAdd = (e:React.FormEvent):void=>{
 e.preventDefault()
+if(todo){
 setTodos([...todos,{id: Date.now(),todo:todo, isDone:false}])
-
+setTodo("")
+console.log("test")
+}
+console.log("test")
   }
+  console.log(todos)
   return (
     <div className="App">
    <span className="heading">Taskify</span>
-   <InputField todo={todo} setTodo={setTodo} handleAdd ={handleAdd }/>
+   <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -5,14 +5,14 @@ todo:string,
 setTodo:React.Dispatch<React.SetStateAction<string>>,
 handleAdd:(e:React.FormEvent)=>void,
 }
-export default function InputField({todo,setTodo, handleAdd}:Props) {
+export default function InputField({todo, setTodo, handleAdd}:Props) {
   return (
-      <div className='input'onSubmit={handleAdd}>
+      <form className='input' onSubmit={handleAdd}>
    <input type="input"
    value={todo}
    onChange={(e)=>setTodo(e.target.value)}
    placeholder='Enter a task' className='input_box'/>
    <button className="input_submit" type="submit">Go</button>
-   </div>
+   </form>
   )
 }
