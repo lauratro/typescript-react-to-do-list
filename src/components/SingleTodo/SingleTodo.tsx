8e7c4af4752cb7d1f18ccc73d1c8671ca1ad931c
lauratro <lauratronchin@hotmail.it>
2022-05-23 15:06:01 +0200

    Add function in draggable

--- src/components/SingleTodo/SingleTodo.tsx
+++ src/components/SingleTodo/SingleTodo.tsx
@@ -36,8 +36,12 @@ const SingleTodo: React.FC<Props> = ({index,todo, todos, setTodos})=>{
   return (
   <Draggable draggableId={todo.id.toString()} index={index}>
 
-      {()=>(
-  <form className="todos__single" onSubmit={(e)=>handleEdit(e,todo.id)}>
+      {(provided)=>(
+  <form className="todos__single" onSubmit={(e)=>handleEdit(e,todo.id)}
+  {...provided.draggableProps}
+  {...provided.dragHandleProps}
+  ref={provided.innerRef}
+  >
 
       {
       edit ?(
